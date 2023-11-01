b403079898a83b422f050dbecc2cbf67316018d4
Sam Wan <sam@wanfamily.org>
2023-11-01 16:15:25 -0700

    added sword 2

--- src/items/Sword.java
+++ src/items/Sword.java
@@ -8,11 +8,10 @@ import game.Item;
 
 public class Sword extends Item implements Melee{
 	public Sword() {
-		GImage image = new GImage("src/images/k00tvpmql6d41.png");
+		GImage image = new GImage("src/images/swordLeft.png");
 		image.setLocation(image.getLocation().getX()-50,image.getLocation().getY()+65);
 
 		image.setSize(100,100);
-		image.rotate(90);
 		this.getItemBody().add(image);
 	}
 
