0f462f884c6add96dcf8d390e6137b209cc68968
Juan <juancuevas2613@gmail.com>
2023-11-13 15:53:12 -0800

    boulder_1 structured terrain

---
+++ src/structures/boulder_1.java
@@ -0,0 +1,17 @@
+package structures;
+
+import acm.graphics.GImage;
+import game.Structure;
+
+public class boulder_1 extends Structure{
+
+	public boulder_1(int x, int y) {
+		super(x, y);
+		
+		GImage boulder_1 = new GImage("media/terrain/boulder_1.png");
+			boulder_1.setLocation(x, y);
+			boulder_1.setSize(150, 150);
+			this.getObjects().add(boulder_1);
+	}
+
+}
