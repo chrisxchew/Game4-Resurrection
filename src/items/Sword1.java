a758b3bc6909dd29b96548b03dcfbd6def749062
Andrew Lewis <a_lewis18@u.pacific.edu>
2023-11-14 10:07:06 -0800

    Fixed Sword Error

--- src/items/Sword1.java
+++ src/items/Sword1.java
@@ -9,8 +9,8 @@ import game.Item;
 public class Sword1 extends Item implements Melee{
 	public Sword1() {
 
-		GImage image = new GImage("media/Items/Weapons/Sword_Left.png");
-		GImage imageRight = new GImage("media/Items/Weapons/Sword_Right.png");
+		GImage image = new GImage("media/Items/Weapons/Sword/Sword_Left.png");
+		GImage imageRight = new GImage("media/Items/Weapons/Sword/Sword_Right.png");
 
 		image.setSize(75,75);
 		imageRight.setSize(75,75);
