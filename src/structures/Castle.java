14efbe6bb90bcb70202a457b0a7632bd1ca4c834
Christopher Chew <christopherchew03@gmail.com>
2023-11-17 14:41:07 -0800

    Added top right collider

--- src/structures/Castle.java
+++ src/structures/Castle.java
@@ -34,6 +34,8 @@ public class Castle extends Structure {
 		this.getColliders().add(r2TopCastle);
 		GLine r3TopCastle = new GLine(713, 71, 745, 101);
 		this.getColliders().add(r3TopCastle);
+		GLine r4TopCastle = new GLine(745, 101, 745, 129);
+		this.getColliders().add(r4TopCastle);
 
 	}
 }
