ff767e8ece2673db8b26b2dae057cc198da9df83
Sam <sam@wanfamily.org>
2023-11-29 18:26:48 -0800

    more main menu stuff

--- src/userinterface/MainMenu.java
+++ src/userinterface/MainMenu.java
@@ -1,24 +1,42 @@
 package userinterface;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+
+import javax.swing.Timer;
 
 import acm.graphics.*;
 import game.Main;
 
-public class MainMenu {
+public class MainMenu implements ActionListener{
     private ArrayList<GObject> objects = new ArrayList<GObject>();
     Main main;
     boolean loadScreen = false;
+    GImage background = new GImage("media/UI/MainMenu/Blue Kirby Background.png");
+    GImage startButton = new GImage("media/UI/MainMenu/start_button.png");
+    GImage quitButton = new GImage("media/UI/MainMenu/quit_button.png");
+    GImage loadButton = new GImage("media/UI/MainMenu/load_button.png");
+    GImage saveButton1 = new GImage("media/UI/MainMenu/save_button1.png");
+    GImage saveButton2 = new GImage("media/UI/MainMenu/save_button2.png");
+    GImage saveButton3 = new GImage("media/UI/MainMenu/save_button3.png");
+    GImage backButton = new GImage("media/UI/MainMenu/back_button.png");
+    GImage title = new GImage("media/UI/MainMenu/Game_4_Title.png");
+
     public MainMenu(Main main) {
         this.main = main;
-        GImage background = new GImage("media/UI/MainMenu/Blue Kirby Background.png");
-        GImage startButton = new GImage("media/UI/MainMenu/start_button.png");
-        GImage quitButton = new GImage("media/UI/MainMenu/quit_button.png");
-        GImage loadButton = new GImage("media/UI/MainMenu/load_button.png");
-        loadButton.scale(6);
-        startButton.scale(6);
-        quitButton.scale(6);
+        loadButton.setSize(225,75);
+        startButton.setSize(225,75);
+        quitButton.setSize(225,75);
+        saveButton1.setSize(225,75);
+        saveButton2.setSize(225,75);
+        saveButton3.setSize(225,75);
+        backButton.setSize(225,75);
+        title.setSize(900,300);
+        title.setLocation(100,-300);
+        Timer timer = new Timer(10, this);
+        timer.start();
         startButton.setLocation(500-startButton.getWidth()/2, 300-startButton.getHeight()/2);
         loadButton.setLocation(500-loadButton.getWidth()/2, 375-loadButton.getHeight()/2);
         quitButton.setLocation(500-quitButton.getWidth()/2, 450-quitButton.getHeight()/2);
@@ -27,60 +45,91 @@ public class MainMenu {
         objects.add(startButton);
         objects.add(loadButton);
         objects.add(quitButton);
-    }
-    public boolean buttonPressed(int x, int y, int width, int height, MouseEvent e) {
-        if(e.getX() > x && e.getX() < x + width && e.getY() > y && e.getY() < y + height) {
-            return true;
-        } else {
-            return false;
-        }
+        objects.add(title);
     }
     public ArrayList<GObject> getObjects() {
         return objects;
     }
+    //every timer tick we lower the title
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(title.getLocation().getY() < 0){
+            title.move(0, 2);
+        }
+    }
     public void keyPressed(MouseEvent e) {
         //buttosn are 225 width
 
-        if(buttonPressed(500-225/2, 300-225/2, 225, 225, e)) {
-            main.startGame();
+        if(loadScreen){
+            if(main.getElementAt(e.getX(), e.getY()) == saveButton1){
+                main.loadGame("save1");
+                return;
+            }else if(main.getElementAt(e.getX(), e.getY()) == saveButton2){
+                main.loadGame("save2");
+                return;
+            }else if(main.getElementAt(e.getX(), e.getY()) == saveButton3){
+                main.loadGame("save3");
+                return;
+            }else if(main.getElementAt(e.getX(), e.getY()) == backButton){
+                remakeMainMenu();
+                return;
+            }
+
         }
-        if(buttonPressed(500-225/2, 375-225/2, 225, 225, e)) {
-            //main.loadGame();
+        if(!loadScreen){
+            if(main.getElementAt(e.getX(), e.getY()) == startButton && !loadScreen){
+                System.out.println("start");
+                main.startGame();
+                return;
+            }else if(main.getElementAt(e.getX(), e.getY()) == quitButton){
+                System.exit(0);
+            }else if(main.getElementAt(e.getX(), e.getY()) == loadButton){
+                makeLoadScreen();
+                return;
+            }
         }
-        if(buttonPressed(500-225/2, 450-225/2, 225, 225, e)) {
-            System.exit(0);
+    }
+    public void removeAllObjectsFromScreen(){
+        for(GObject object: objects){
+            main.remove(object);
         }
     }
     public void makeLoadScreen(){
-        loadScreen  = true;
+
+        removeAllObjectsFromScreen();
         objects.clear();
-        GImage background = new GImage("media/UI/MainMenu/Blue Kirby Background.png");
-        GImage loadScreen = new GImage("media/UI/MainMenu/load_screen.png");
-        loadScreen.scale(6);
-        loadScreen.setLocation(500-loadScreen.getWidth()/2, 250-loadScreen.getHeight()/2);
-        background.setSize(1000,500);
         objects.add(background);
-        objects.add(loadScreen);
+
+
+        saveButton1.setLocation(500-saveButton1.getWidth()/2, 300-saveButton1.getHeight()/2);
+        saveButton2.setLocation(500-saveButton2.getWidth()/2, 375-saveButton2.getHeight()/2);
+        saveButton3.setLocation(500-saveButton3.getWidth()/2, 450-saveButton3.getHeight()/2);
+        //put back button at top left
+        backButton.setLocation(10,10);
+        objects.add(saveButton1);
+        objects.add(saveButton2);
+        objects.add(saveButton3);
+        objects.add(backButton);
+        main.loadMainMenu();
+        loadScreen  = true;
     }
     public void remakeMainMenu(){
-        loadScreen = false;
+
+        removeAllObjectsFromScreen();
         objects.clear();
-        GImage background = new GImage("media/UI/MainMenu/Blue Kirby Background.png");
-        GImage startButton = new GImage("media/UI/MainMenu/start_button.png");
-        GImage quitButton = new GImage("media/UI/MainMenu/quit_button.png");
-        GImage loadButton = new GImage("media/UI/MainMenu/load_button.png");
-        loadButton.scale(6);
-        startButton.scale(6);
-        quitButton.scale(6);
+        title.setLocation(100,-300);
+
         startButton.setLocation(500-startButton.getWidth()/2, 300-startButton.getHeight()/2);
         loadButton.setLocation(500-loadButton.getWidth()/2, 375-loadButton.getHeight()/2);
         quitButton.setLocation(500-quitButton.getWidth()/2, 450-quitButton.getHeight()/2);
-        background.setSize(1000,500);
+
         objects.add(background);
         objects.add(startButton);
         objects.add(loadButton);
         objects.add(quitButton);
+        objects.add(title);
         main.loadMainMenu();
+        loadScreen  = false;
     }
 
 }
