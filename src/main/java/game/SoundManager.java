4eb238d01c742af3105c149b9520643de40e8add
Christopher Chew <christopherchew03@gmail.com>
2025-01-27 22:19:27 -0800

    Added 4 more sounds into SoundManager and respective sound folder

--- src/main/java/game/SoundManager.java
+++ src/main/java/game/SoundManager.java
@@ -2,7 +2,17 @@ package game;
 
 public class SoundManager {
 	public static final String SOUND = "sound";
-	public static final String MUSIC = "Matroda - Gimme Some Keys  Insomniac Records.mp3";
+	public static final String MENU = "Matroda - Gimme Some Keys  Insomniac Records.mp3";
+	public static final String HIT = "Fall Damage Health Hit (Nr. 2  Fortnite Sound) - Sound Effect for editing.mp3";
+	public static final String DUNGEON = "Undertale OST꞉ 006 - Uwa!! So Temperate♫.mp3";
+	public static final String GAME = "Virtual Paradise.mp3";
+	public static final String SWING = "Swing Sound  Popular Free Sound Effects  #copyrightfree  [ Copyright Free Gun & Weapon Sounds ].mp3";
+	public static final String FIRE = "Minecraft Fireball Sound Effect.mp3";
+	public static final String ARROW = "BowArrow Shot  Sound Effect.mp3";
+	public static final String ENEMY = "Minecraft - Slime Big (Nr. 3) - Sound Effect  Best Quality #Shorts.mp3";
+	public static final String CLICK = "Wooden Button Click Sound Effect.mp3";
+
+
 	AudioPlayer audio;
 	public void init() {
 		audio = AudioPlayer.getInstance();
@@ -10,13 +20,34 @@ public class SoundManager {
 	public void playSound(String sound) {
 		switch(sound) {
 		case "music":
-			audio.playSound(SOUND, MUSIC);
+			audio.playSound(SOUND, MENU);
 			break;
-		/*case "damaged":
-			audio.playSound(SOUND, );
+		case "game":
+			audio.playSound(SOUND, GAME);
 			break;
-		case */
+		case "hit":
+			audio.playSound(SOUND, HIT);
+			break;
+		case "dungeon":
+			audio.playSound(SOUND, DUNGEON);
+			break;
+		case "swing":
+			audio.playSound(SOUND, SWING);
+			break;
+		case "fire":
+			audio.playSound(SOUND, FIRE);
+			break;
+		case "arrow":
+			audio.playSound(SOUND, ARROW);
+			break;
+		case "enemy":
+			audio.playSound(SOUND, ENEMY);
+			break;
+		case "click":
+			audio.playSound(SOUND, CLICK);
 		}
+		
+		
 	}
 }
 
